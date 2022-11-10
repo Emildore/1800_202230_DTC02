9a6e8da821a5cbe2105736553cbc9054553e0b73
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-11-09 22:14:21 -0800

    add comments

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,3 +1,4 @@
+// This function stores the data from API to Firestore, data is already stores so it's commented out
 // var listQuotes = {}
 // //https://stoicquotesapi.com/quotes
 // setup = function () {
@@ -6,22 +7,15 @@
 //             url: "https://stoicquotesapi.com/v1/api/quotes?page=5",
 //             type: "GET",
 //             success: function (quotesList) {
-//                 // for (i = 0; i < data.results.length; i++) {
 //                 quotesList.data.forEach(eachQuotes => {
-//                     // listQuotes[eachQuotes.author] = eachQuotes.body
-//                     // console.log(listQuotes)
 
-//                     //define a variable for the collection you want to create in Firestore to populate data
+//                     //defining a variable for the collection i want to create in Firestore to populate data
 //                     var quotesRef = db.collection("quotes");
 
 //                     quotesRef.add({
-//                         quote: eachQuotes.body,
-//                         author: eachQuotes.author,    //replace with your own city?
-//                         // last_updated: firebase.firestore.FieldValue.serverTimestamp()
+//                         quote: eachQuotes.body, // adding the quote to Firestore
+//                         author: eachQuotes.author, //adding the author to Firestore
 //                     });
-
-
-
 //                 });
 //             },
 //             error: function (error) {
@@ -32,19 +26,24 @@
 // }
 // $(document).ready(setup);
 
+// this function retrieves the data from firestore and populates it into html using peudo randomizer
 function displayQuotes() {
+    // initializing these values for a function later
     let random_int = Math.floor(Math.random() * 74);
     let x = 0
     let y = 0
+    // grabbing the data from firestore
     db.collection("quotes").get()
-        .then(all_ids => {
+        .then(snap => {
 
-            all_ids.forEach(doc => { //iterate thru each doc
+            snap.forEach(doc => { //iterate thru each doc
+                // below code peudo randomizes the quotes that are being pulled through firestore
                 x++
                 if (x == random_int) {
 
-                    var quote = doc.data().quote;        // get value of the "name" key
+                    var quote = doc.data().quote;
                     var author = doc.data().author;
+                    // these codes allow the values from firestore to be injected in html
                     $(`#random_quote${y}`).html(quote)
                     $(`#random_author${y}`).html(author)
 

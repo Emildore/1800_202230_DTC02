e791fe220b45accd5a6fb67578b828b74f88e104
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2022-11-20 10:44:08 -0800

    Update firebaseAPI_template.js

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -1,14 +1,24 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
- apiKey: "AIzaSyBp7W8iv5fgxQgrpfXFhKfgKB12SqN03xQ",
-  authDomain: "fir-ca432.firebaseapp.com",
-  projectId: "fir-ca432",
-  storageBucket: "fir-ca432.appspot.com",
-  messagingSenderId: "1086739998382",
-  appId: "1:1086739998382:web:d21460935b287b0df1af3f"
+const firebaseConfig = {
+
+  apiKey: "AIzaSyC9X1-phP90RRp0E63tDMXIym_CmsnMEpU",
+
+  authDomain: "motivational-app-6a564.firebaseapp.com",
+
+  projectId: "motivational-app-6a564",
+
+  storageBucket: "motivational-app-6a564.appspot.com",
+
+  messagingSenderId: "621250893228",
+
+  appId: "1:621250893228:web:3e687a070f4ccf932f9609",
+
+  measurementId: "G-3009NRPSBN"
+
 };
+
 
 //--------------------------------------------
 // initialize the Firebase app
